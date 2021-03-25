7f247e9141b0cb1ff3192621a7fd57bfcd48a420
Alexander Zhariuk <codo6@mail.ru>
2021-03-25 17:01:49 +0300

    Добавил выдачу местоположения при обращении к API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from iss.iss import get_iss_location
+import json
 
 app = Flask('__main__')
 
@@ -6,6 +8,13 @@ app = Flask('__main__')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/api/iss/')
+def getting_iss_location():
+    iss_position_data = get_iss_location()
+    iss_position_data_json = json.dumps(iss_position_data)
+    return iss_position_data_json
 
 
 if __name__ == '__main__':
